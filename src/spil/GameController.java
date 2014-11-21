84aaaa7eedf48275bac4faa970754d4249111bec
tobiasjulkastrup <s134225@student.dtu.dk>
2014-11-21 09:43:06 +0100

    Tilføjet totalPlayers() til GameController

--- src/spil/GameController.java
+++ src/spil/GameController.java
@@ -13,11 +13,16 @@ public class GameController {
 		iCO.setGUI();
 	
 		GUI.showMessage(ICO.messages.getString("firstMessageGUI")); // "Welcome. Press ok to start the game."
-		numbersOfPlayers = GUI.getUserInteger(ICO.messages.getString("numbersOfPlayersGUI")); // "How many players will play the game?"
+		numberOfPlayers = GUI.getUserInteger(ICO.messages.getString("numbersOfPlayersGUI")); // "How many players will play the game?"
 		
 		// Add players to the game and on the board
 		Player player = new Player();
-		for (int i = 0; i < numbersOfPlayers; i++) {
+		
+		// Sents total players to Player()
+		player.totalPlayers(numberOfPlayers);
+		
+		// Adds as many players as numberOfPlayers
+		for (int i = 0; i < numberOfPlayers; i++) {
 			player.newPlayer(GUI.getUserString(ICO.messages.getString("inputNameGUI")+(i+1)+"?")); //"What is your name, Player 
 		}
 	}
