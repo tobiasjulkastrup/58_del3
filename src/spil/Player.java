1340426f221f987f0e220c2252f3cfec4abaf384
Emil <Emil@10.16.167.170>
2014-11-21 09:40:41 +0100

    retter spillernavn

--- src/spil/Player.java
+++ src/spil/Player.java
@@ -1,10 +1,19 @@
 package spil;
 
 public class Player {
+	java.util.Scanner input = new java.util.Scanner(System.in);
+	int totalPlayers;
+	public void totalPlayers(int numberOfPlayers){
+		totalPlayers=numberOfPlayers;
+	}
 	
-	String name;
-	
-	public void newPlayer (String name) {
-		
+	public String newPlayer (String name) {
+		String[] PlayerName = new String[totalPlayers];	
+		for (int i=0; i<=totalPlayers; i++){
+			System.out.println("spiller"+(i+1)+" indtast navn");
+			
+			PlayerName[i]=input.nextLine();
+			}		
+			return PlayerName.toString(); //returnerer string
 	}
 }
