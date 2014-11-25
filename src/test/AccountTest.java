f71b28abf5598db7ae2b02846664a6c60c66dc3a
Dtu Andreas <s082853@student.dtu.dk>
2014-11-25 16:14:12 +0100

    AccountTest - lavet Testdeposit og Testwithdraw

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -2,18 +2,46 @@ package test;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
+
+import spil.Account;
 
 public class AccountTest {
 
+
+	//
 	@Test
+
 	public void testDeposit() {
-		fail("Not yet implemented");
+
+		// preconditions
+
+		// sætter balancen til 1000
+		Account account;
+		account = new Account();
+		account.setAccount(1000);
+
+		// test
+		account.deposit(500);
+		// postcondition
+		assertEquals(1500, account.getBalance());
 	}
 
 	@Test
 	public void testWithdraw() {
-		fail("Not yet implemented");
-	}
+		// preconditions
 
+		// sætter balancen til 1000
+		Account account;
+		account = new Account();
+		account.setAccount(1000);
+
+		// test
+		account.withdraw(-500);
+		// postcondition
+		assertEquals(500, account.getBalance());
+
+	}
 }
+
