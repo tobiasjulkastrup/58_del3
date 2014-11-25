de700d2d104b13559f02aaa4166bbd44c409f5bc
tobiasjulkastrup <s134225@student.dtu.dk>
2014-11-25 11:53:20 +0100

    Tilføjet get og set metoder til DiceCup + en metoder der udregner det samlede slag.

--- src/spil/DiceCup.java
+++ src/spil/DiceCup.java
@@ -2,16 +2,37 @@ package spil;
 
 public class DiceCup {
 
-	public int[] getDiceCup(int dieRolls, int eyesOnDice){
-		int eyes = eyesOnDice;
-		int rolls = dieRolls;
-		int[] diceArray = new int[rolls];
+	private int[] diceArray;
+	private int rolls;
+	
+	public void setDiceCup(int diceRolls, int eyesOnDice) {
+		
+		diceArray = new int[diceRolls];
+		rolls = diceRolls;
 		
 		Dice dice = new Dice();
 		
 		for (int i = 0; i < rolls; i++) {
-			diceArray[i] = dice.getDice(eyes);
+			diceArray[i] = dice.getDice(eyesOnDice);
 		}
-		return diceArray;
+		
 	}
+	
+	public int[] getDiceCup() {
+		
+		return diceArray;
+		
+	}
+	
+	public int getTotalDiceCup() {
+		
+		int totalDiceValue = 0;
+		
+		for (int i = 0; i < rolls; i++) {
+			totalDiceValue =+ diceArray[i];
+		}
+		
+		return totalDiceValue;
+	}
+	
 }
