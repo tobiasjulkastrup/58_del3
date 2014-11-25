b530d5c0159f8132688b8e4783208ee1e200cf7f
Oliver <Oliver@10.16.174.183>
2014-11-25 17:16:56 +0100

    Tilføjet variabler og løkker

--- src/board/Tax.java
+++ src/board/Tax.java
@@ -1,19 +1,35 @@
 package board;
 
+import boundaryToMatador.GUI;
+import spil.ICO;
 import spil.Player;
 
 public class Tax extends Field {
-
-	public Tax(String name) {
+	private int taxAmount;
+	private int taxRate;
+	
+	public Tax(String name, int taxAmount, int taxRate) {
 		super(name);
+		this.taxAmount = taxAmount;
+		this.taxRate = taxRate;
 		
 	}
-	private int taxAmount;
-	private int taxRate = -1;
+	
+	
+	
 	@Override
 	public void landOnField(Player player) {
 		
-		
+		if (taxRate < 0) { 
+			//brug taxAmount
+			player.withdraw(taxAmount);
+		}
+		else
+			//Udregn taxRate ud fra gem i int variable
+			player.getBalance();
+			//Spørg om man vil betale taxRate eller taxAmount
+			GUI.getUserButtonPressed("taxChoice", "Tax Rate", "TaxAmount")
+			//Find ud af hvilken knap der bliver trykket på, og udfør withdraw efter hvad der bliver valgt.
 	}
 	
 }
