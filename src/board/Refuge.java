be33660e3f77240e0a42fce62acd241f2b34d334
Oliver <Oliver@OliversPC.hhk.dk>
2014-11-26 14:35:01 +0100

    Refuge færdig

--- src/board/Refuge.java
+++ src/board/Refuge.java
@@ -1,10 +1,11 @@
 package board;
 
+import boundaryToMatador.GUI;
 import spil.Player;
 
 public class Refuge extends Field {
 
-public Refuge(String name) {
+public Refuge(String name, int bonus) {
 		super(name);
 	
 	}
@@ -14,7 +15,8 @@ private int bonus;
 @Override
 public void landOnField(Player player) {
 	
-	
+	player.deposit(bonus);
+	GUI.showMessage("Du er landet på feltet " + getName() + "og modtager " + bonus );
 }
 	
 }
