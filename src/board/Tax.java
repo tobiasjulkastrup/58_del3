85e15683d3622cbb9aaff42fd94127fe7631b937
Oliver <Oliver@10.84.21.52>
2014-11-28 00:46:31 +0100

    Oprydning i koden

--- src/board/Tax.java
+++ src/board/Tax.java
@@ -1,8 +1,7 @@
 package board;
 
-import boundaryToMatador.GUI;
-import spil.ICO;
 import spil.Player;
+import boundaryToMatador.GUI;
 
 public class Tax extends Field {
 	private int taxAmount;
@@ -19,21 +18,19 @@ public class Tax extends Field {
 	public void landOnField(Player player) {
 
 		if (taxRate < 0) {
-			// brug taxAmount
+
 			player.withdraw(taxAmount);
 		} else {
 			int taxratetopay = player.getBalance() / 100 * 10;
 			String taxRateChoiceString;
 			boolean taxRateBool = false;
 
-			// Spørg om man vil betale taxRate eller taxAmount
 			taxRateChoiceString = GUI
 					.getUserButtonPressed(
 							"Du skal betale skat. Vil du betale en fast takst eller 10% af din pengebeholdning?",
 							"Fast takst" + taxAmount, "10 % af dine penge"
 									+ taxratetopay);
-			// Find ud af hvilken knap der bliver trykket på, og udfør withdraw
-			// efter hvad der bliver valgt.
+
 			taxRateBool = taxRateChoiceString.equals("Fast takst" + taxAmount);
 
 			if (taxRateBool = true) {
