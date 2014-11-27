ad8a37a454cf249e5a1f815ead214eefc0194223
tobiasjulkastrup <s134225@student.dtu.dk>
2014-11-27 23:16:55 +0100

    Formaterings ændringer.

--- src/board/Field.java
+++ src/board/Field.java
@@ -1,23 +1,23 @@
 package board;
+
 import spil.Player;
 
 public abstract class Field {
 
 	private String name;
-	
-	
+
 	public abstract void landOnField(Player player);
 
 	public Field(String name) {
-		
+
 		this.name = name;
 
 	}
-	
+
 	public String getName(){
-		
+
 		return name;
-		
+
 	}
 
 }
