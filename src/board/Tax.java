4f4d5db5ed74c3a8105517804016bdfdb26ac02b
Oliver <Oliver@10.84.21.52>
2014-11-28 00:44:30 +0100

    Færdiggjort

--- src/board/Tax.java
+++ src/board/Tax.java
@@ -7,27 +7,40 @@ import spil.Player;
 public class Tax extends Field {
 	private int taxAmount;
 	private int taxRate;
-	
+
 	public Tax(String name, int taxAmount, int taxRate) {
 		super(name);
 		this.taxAmount = taxAmount;
 		this.taxRate = taxRate;
-		
+
 	}
-	
+
 	@Override
 	public void landOnField(Player player) {
-		
-		if (taxRate < 0) { 
-			//brug taxAmount
+
+		if (taxRate < 0) {
+			// brug taxAmount
 			player.withdraw(taxAmount);
+		} else {
+			int taxratetopay = player.getBalance() / 100 * 10;
+			String taxRateChoiceString;
+			boolean taxRateBool = false;
+
+			// Spørg om man vil betale taxRate eller taxAmount
+			taxRateChoiceString = GUI
+					.getUserButtonPressed(
+							"Du skal betale skat. Vil du betale en fast takst eller 10% af din pengebeholdning?",
+							"Fast takst" + taxAmount, "10 % af dine penge"
+									+ taxratetopay);
+			// Find ud af hvilken knap der bliver trykket på, og udfør withdraw
+			// efter hvad der bliver valgt.
+			taxRateBool = taxRateChoiceString.equals("Fast takst" + taxAmount);
+
+			if (taxRateBool = true) {
+				player.withdraw(taxAmount);
+			} else
+				player.withdraw(taxratetopay);
 		}
-		else
-			//Udregn taxRate ud fra gem i int variable
-			player.getBalance();
-			//Spørg om man vil betale taxRate eller taxAmount
-			GUI.getUserButtonPressed("taxChoice", "Tax Rate", "TaxAmount");
-			//Find ud af hvilken knap der bliver trykket på, og udfør withdraw efter hvad der bliver valgt.
+
 	}
-	
 }
