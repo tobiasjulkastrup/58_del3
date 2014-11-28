c14c4ae9920890ce5f92fea43e2bd9598c5c787b
Emil <Emil@10.16.170.87>
2014-11-28 14:43:40 +0100

    rettet test

--- src/test/TaxTest.java
+++ src/test/TaxTest.java
@@ -7,6 +7,7 @@ import java.awt.Color;
 import org.junit.Test;
 
 import board.Tax;
+import spil.ICO;
 import spil.Player;
 import board.GameBoard;
 import spil.GameController;
@@ -17,7 +18,10 @@ public class TaxTest {
 		//Preconditions
 		Tax tax;
 		//opretter gameboard
-		GameBoard gameboard = new GameBoard(null);
+		ICO iCO = new ICO();
+		iCO.setLang("da", "DK");
+		GameController gameController = new GameController();
+		GameBoard gameboard = new GameBoard(gameController);
 		// sætter tax
 		tax= new Tax(null, 500, 1);
 		// opretter player
