fa4d37e905759afc2e3d37708d60d899b729f78c
Dtu Andreas <s082853@student.dtu.dk>
2014-11-28 09:18:36 +0100

    Rettet AccountTest

--- src/test/DiceCupTest.java
+++ src/test/DiceCupTest.java
@@ -1,18 +1,23 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+
+import spil.DiceCup;
 
 public class DiceCupTest {
 
 	@Test
 	public void testGetDiceCup() {
 		
-		//DiceCup test
-//		DiceCup diceCup = new DiceCup();
-//		int[] dice;
-//		dice = diceCup.getDiceCup(2, 6);
+		DiceCup test;
+		DiceCup diceCup = new DiceCup();
+		int[] dice;
+		diceCup.setDiceCup(2, 6);
+		
+		
+		
 //		System.out.println(""+dice[0]+" "+dice[1]);
 		
 		fail("Not yet implemented");
