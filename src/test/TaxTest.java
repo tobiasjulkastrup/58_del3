74671a475451e9a714c2a6867805c934aae41744
Emil <Emil@10.16.170.87>
2014-11-28 12:02:16 +0100

    Test af tax lavet

--- src/test/TaxTest.java
+++ src/test/TaxTest.java
@@ -9,19 +9,22 @@ import org.junit.Test;
 import board.Tax;
 import spil.Player;
 import board.GameBoard;
-
+import spil.GameController;
 public class TaxTest {
 
 	@Test
 	public void testLandOnField() {
 		//Preconditions
 		Tax tax;
-		GameBoard gameboard;
-		gameboard= new GameBoard();
+		//opretter gameboard
+		GameBoard gameboard = new GameBoard(null);
+		// sætter tax
 		tax= new Tax(null, 500, 1);
+		// opretter player
 		Player player;
 		player= new Player();
 		player.setPlayer("John", 1000, null);		
+		
 		//Test for fast taxrate
 		gameboard.fields[17].landOnField(player);
 		//postconditions
@@ -29,9 +32,8 @@ public class TaxTest {
 		
 		//Test for 10%
 		gameboard.fields[17].landOnField(player);
+		//post conditions
 		assertEquals(450, player.getBalance());
-		
-		//fail("Not yet implemented");
 	}
 
 //	@Test
